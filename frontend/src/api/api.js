847b4fda2e06351323ce46f477a04db74d65090e
sheshakiaq <shesha.kiaq@gmail.com>
2026-09-26 16:13:08 +0530

    api url updated 2

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.smarttask.local:5000",
+  baseURL: "https://localhost:5000/api/auth/login",
   headers: {
     "Content-Type": "application/json",
   },
